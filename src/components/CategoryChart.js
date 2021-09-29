6d88f9feede4d3d6258d7b4e3dd0c8d471495336
Anika Bernstein <anikajb92@gmail.com>
2021-09-29 00:07:15 -0600

    categorychart styled

--- src/components/CategoryChart.js
+++ src/components/CategoryChart.js
@@ -6,8 +6,8 @@ export default function CategoryChart(props) {
   return (
     <div>
       <Chart
-        width={'500px'}
-        height={'300px'}
+        width={'350px'}
+        height={'350px'}
         chartType="PieChart"
         loader={<div>Loading Chart</div>}
         data={[['Category', 'Number of Tasks Per'], ...props.data]}
@@ -16,6 +16,13 @@ export default function CategoryChart(props) {
           pieSliceText: 'label',
           title: 'Number of Tasks Per Category',
           pieStartAngle: 100,
+          slices: {
+            0: { color: '#FF416C' }, // pink
+            1: { color: '#FF4B2B' }, // orange
+            2: { color: '#FFB627' }, // yellow
+            3: { color: '#00A6A6' },  // blue
+            4: { color: '#F7EDF0' } // 
+          }
         }}
         rootProps={{ 'data-testid': '4' }}
       />
