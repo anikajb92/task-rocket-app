b1c2293b8b76b84f887ede07933e64526c4470ed
Anika Bernstein <anikajb92@gmail.com>
2021-09-29 00:57:02 -0600

    Added and styled progress bar

--- src/components/CategoryChart.js
+++ src/components/CategoryChart.js
@@ -4,12 +4,12 @@ import { Chart } from "react-google-charts";
 export default function CategoryChart(props) {
 
   return (
-    <div>
+    <div className="chart">
       <Chart
-        width={'350px'}
-        height={'350px'}
+        width={'200px'}
+        height={'200px'}
         chartType="PieChart"
-        loader={<div>Loading Chart</div>}
+        loader={<div>Preparing for Liftoff...</div>}
         data={[['Category', 'Number of Tasks Per'], ...props.data]}
         options={{
           legend: 'none',
@@ -22,7 +22,18 @@ export default function CategoryChart(props) {
             2: { color: '#FFB627' }, // yellow
             3: { color: '#00A6A6' },  // blue
             4: { color: '#F7EDF0' } // 
-          }
+          }, 
+          is3D: true, 
+          fontName: "Montserrat", 
+          chartArea: {
+            width: "100%",
+            height: "100%"
+          }, 
+          backgroundColor: "transparent",
+          textStyle: {
+              color: "233238",
+              fontSize: 20
+            }
         }}
         rootProps={{ 'data-testid': '4' }}
       />
