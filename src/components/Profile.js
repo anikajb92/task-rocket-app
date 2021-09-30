80926dbf03eb757e0a4ac26a9d7313ea5f5ec393
Anika Bernstein <anikajb92@gmail.com>
2021-09-29 23:08:59 -0600

    profile page now offering user data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import {Link} from 'react-router-dom';
-
+import {BsClockHistory} from 'react-icons/bs';
 
 export default function Profile(props) {
 
@@ -9,11 +9,13 @@ export default function Profile(props) {
         {props.isLoggedIn? (
         <>
           <h1> Hi, {props.user.firstname}! </h1>
+          <p><BsClockHistory /> Thank you for being a loyal patron of Task Rocket. Your acount has been active for <span>{props.user.user_active}</span> hours.</p>
+
           <h2>Here's a look at your profile. Need to make changes?</h2>
           <hr/>
-          <h3>Full Name: {props.user.firstname} {props.user.lastname}</h3>
-          <h3>Username: {props.user.username}</h3>
-          <h3>Password: (Currently Unavailable)</h3>
+          <h3>Full Name: <p>{props.user.firstname} {props.user.lastname}</p></h3>
+          <h3>Username: <p>{props.user.username}</p></h3>
+          <h3>Password: <p>(Currently Unavailable)</p></h3>
           <h3>Profile Picture: </h3>
           <button>Currently Unavailable</button>
           <hr/>
