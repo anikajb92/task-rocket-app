d56a82ed2c9b5a40d260919b7ccf5922854a75d1
Anika Bernstein <anikajb92@gmail.com>
2021-10-11 21:04:11 -0600

    pink rocket showing up on about page

--- src/components/About2.js
+++ src/components/About2.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
 
 export default function About2() {
   return (
     <div>
       <p>Hello</p>
+      <Player
+        autoplay
+        loop
+        src="https://assets2.lottiefiles.com/packages/lf20_ikwwxokt.json"
+        style={{ height: '500px', width: '500px' }}
+      >
+      </Player>
     </div>
   )
 }
