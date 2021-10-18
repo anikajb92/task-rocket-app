6263a7c5d3f1efd168235a668877b72659f756e9
Anika Bernstein <anikajb92@gmail.com>
2021-10-18 15:19:03 -0600

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import {IoRocketOutline} from "react-icons/io5";
 
 export default function Footer() {
   return (
-    <div>
-      <p>Footer Here</p>
+    <div className="footer">
+      <p>Built by Anika Bernstein 2021. All Rights Reserved</p>
     </div>
   )
 }
